26cbdfab29f9365aea99efb5487ea0b7c7d84423
Lakshya Vipassana <lakshyakgp.24@kgpian.iitkgp.ac.in>
2026-05-28 17:54:19 +0530

    Update Revenue_Analysis.py

--- pages/Revenue_Analysis.py
+++ pages/Revenue_Analysis.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.title("📈 Revenue Trends")
 
-df = pd.read_csv("/Users/lakshyasmac/Desktop/E-commerce dataset/archive/merged_olist.csv")
+df = pd.read_csv("archive/merged_olist.csv")
 
 df['order_purchase_timestamp'] = pd.to_datetime(
     df['order_purchase_timestamp']
