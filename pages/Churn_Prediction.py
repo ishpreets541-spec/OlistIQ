5abc47dc145f6df86998f6b33c937018e17515cc
Lakshya Vipassana <lakshyakgp.24@kgpian.iitkgp.ac.in>
2026-05-28 17:53:26 +0530

    Update Churn_Prediction.py

--- pages/Churn_Prediction.py
+++ pages/Churn_Prediction.py
@@ -22,7 +22,7 @@ st.title("🔮 Churn / Repeat Purchase Prediction")
 # Load Data
 # =========================================
 
-df = pd.read_csv("/Users/lakshyasmac/Desktop/E-commerce dataset/archive/merged_olist.csv")
+df = pd.read_csv("archive/merged_olist.csv")
 
 # =========================================
 # Datetime
